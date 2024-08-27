12be952cb5581500e5a169a4e4c9b25afc0bf704
Lorena Fuentes <lorena.fuentes.es@gmail.com>
2024-08-26 23:55:04 -0400

    se actualiza codigo

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-variables
+const textarea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat" 
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textarea.value)
+    mensaje.value = textoEncriptado
+}
+
+function encriptar(){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    StringEncriptada = StringEncriptada.toLoweCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(StringEncriptada.includes(matrizCodigo[i][0])){
+        StringEncriptada = StringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+    }
+
+}
+}
